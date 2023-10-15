5beb8c4b5f91c412c8f7492505e554a611ff0fea
Robin Jünemann <robin@juenemanns.de>
2023-10-15 20:42:27 +0200

    added the target email

--- med-website/src/app/app.module.ts
+++ med-website/src/app/app.module.ts
@@ -5,6 +5,7 @@ import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
 import { AlertModule } from '@coreui/angular';
 
+
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PagePraxisComponent } from './page-praxis/page-praxis.component';
--- med-website/src/app/email.service.ts
+++ med-website/src/app/email.service.ts
@@ -1,18 +1,22 @@
 import { Injectable } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmailService {
 
-  private emailUrl = 'http://localhost:3000/send-email'; // Replace with your backend URL
+  private emailUrl = 'http://localhost:8080';
 
   constructor(private http: HttpClient) { }
 
-  sendEmail(name: string, email: string, subject: string, message: string) {
+  sendEmail(name: string, reciever: string, email: string, subject: string, message: string) {
     const data = {
       name: name,
+      reciever: "robin@juenemanns.de",
       email: email,
       subject: subject,
       message: message
