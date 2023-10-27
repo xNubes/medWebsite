f681421b753ee4974f2a8545dff2999888257c54
Robin Jünemann <robin@juenemanns.de>
2023-10-27 16:25:21 +0200

    tried to build a enlarge picture function

--- med-website/src/app/page-location/page-location.component.ts
+++ med-website/src/app/page-location/page-location.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./page-location.component.scss']
 })
 export class PageLocationComponent {
+  
+  enlargeImage(myModal, myImg, myCaption){
+    var modal = document.getElementById(myModal);
+    var modalImg = document.getElementById(myImg);
+    var captionText = document.getElementById(myCaption);
+  
+    modal.style.display = "block";
+    // modalImg.src = this.src;
+    // captionText.innerHTML = this.alt;
+  }
+  
+  exitImage(myModal) {
+    var modal = document.getElementById(myModal);
+  
+    modal.style.display = "none";
+  }
 
 }
+
+
+
